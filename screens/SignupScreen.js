b91a412de754c568d265e9c85d2e0d857fa0c61a
Rachel Skrenta <rachelskrenta@gmail.com>
2022-02-07 13:41:32 -0800

    Created a separate screen for signing up

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,22 +1,35 @@
 import React, {  useState } from 'react';
-import { useNavigation } from '@react-navigation/native';
 import { KeyboardAvoidingView, Text, View, TextInput, TouchableOpacity, Alert } from 'react-native';
 
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+import { doc, setDoc } from "firebase/firestore"; 
 
 import { auth, db } from '../firebase';
 import styles from '../styles/styles.js';
 
-const LoginScreen = () => {
+const SignupScreen = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const navigation = useNavigation();
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
 
-    const handleLogin = () => {
-        signInWithEmailAndPassword(auth, email, password)
+    const handleSignUp = () => {
+        if (firstName === '' || lastName === '') {
+            Alert.alert('Please enter your first and last name');
+            return;
+        }
+        createUserWithEmailAndPassword(auth, email, password)
             .then(userCredentials => {
                 const user = userCredentials.user;
-                console.log('User just logged in with:', user.email);
+                console.log('A new user just registered:', user.email);
+
+                return setDoc(doc(db, 'users', user.uid), {
+                    firstName: firstName,
+                    lastName: lastName,
+                    attending: [],
+                    hosting: []
+                })
             })
             .catch(error => alert(error.message))
     }
@@ -34,6 +47,18 @@ const LoginScreen = () => {
             </View>
             <View style={styles.inputContainer}>
                 <TextInput
+                    placeholder='First name'
+                    value={firstName}
+                    onChangeText={text => setFirstName(text)}
+                    style={styles.input}
+                ></TextInput>
+                <TextInput
+                    placeholder='Last name'
+                    value={lastName}
+                    onChangeText={text => setLastName(text)}
+                    style={styles.input}
+                ></TextInput>
+                <TextInput
                     placeholder='Email'
                     value={email}
                     onChangeText={text => setEmail(text)}
@@ -49,29 +74,14 @@ const LoginScreen = () => {
             </View>
             <View style={styles.buttonContainer}>
                 <TouchableOpacity
-                    onPress={handleLogin}
-                    style={styles.button}
-                >
-                    <Text style={styles.buttonText}>Login</Text>
-                </TouchableOpacity>
-
-
-                <TouchableOpacity
-                    onPress={() => {navigation.push("Signup")}}
+                    onPress={handleSignUp}
                     style={[styles.button, styles.buttonOutline]}
                 >
                     <Text style={styles.buttonOutlineText}>Sign up</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity
-                    onPress={() => {navigation.push("ForgotPassword")}}
-                    style={styles.resetButton}
-                >
-                    <Text style={styles.resetButtonText}>Forgot Password?</Text>
                 </TouchableOpacity>
             </View>
         </KeyboardAvoidingView>
     );
 };
 
-export default LoginScreen;
+export default SignupScreen;
